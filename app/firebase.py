3581735b49c5e3093981a2bdc86a52d4e401c845
eunseo0903 <alice5808@naver.com>
2026-06-12 19:19:35 +0900

    파이어베이스 환경 변수

--- app/firebase.py
+++ app/firebase.py
@@ -1,3 +1,5 @@
+import json
+
 import firebase_admin
 from firebase_admin import credentials
 
@@ -6,5 +8,8 @@ from app.config import settings
 
 def init_firebase() -> None:
     if not firebase_admin._apps:
-        cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+        if settings.FIREBASE_CREDENTIALS_JSON:
+            cred = credentials.Certificate(json.loads(settings.FIREBASE_CREDENTIALS_JSON))
+        else:
+            cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
         firebase_admin.initialize_app(cred)
